80c67348911041cf7747232c3afdd97f9061df21
Unknown <joeyskuo@gmail.com>
2017-09-09 17:20:19 -0700

    added categories tab content

--- admin/categories.php
+++ admin/categories.php
@@ -1,5 +1,4 @@
 <?php include "../includes/db.php" ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -34,8 +33,6 @@
 <body>
 
     <div id="wrapper">
-
-    <?php if($connection) echo "conn"; ?>
         <!-- Navigation -->
         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
             <!-- Brand and toggle get grouped for better mobile display -->
@@ -53,17 +50,48 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Blank Page
-                            <small>Subheading</small>
+                            Welcome to admin
+                            <small>Author</small>
                         </h1>
-                        <ol class="breadcrumb">
-                            <li>
-                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                            </li>
-                            <li class="active">
-                                <i class="fa fa-file"></i> Blank Page
-                            </li>
-                        </ol>
+                        <div class="col-xs-6">
+                          <form action="" method="post">
+                            <div class="form-group">
+                              <label for="cat-title">Add Category</label>
+                              <input class="form-control" type="text" name="cat_title">
+                            </div>
+                            <div class="form-group">
+                              <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+                            </div>
+                          </form>
+
+                        </div>
+                        <div class="col-xs-6">
+                          <?php
+                          $query = "SELECT * FROM categories";
+                          $select_categories = mysqli_query($connection,$query); ?>
+
+                          <table class="table table-bordered table-hover">
+                            <thead>
+                              <tr>
+                                <th>Id</th>
+                                <th>Category Title</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <?php
+                              while($row = mysqli_fetch_assoc($select_categories)) {
+                                $cat_id = $row['cat_id'];
+                                $cat_title = $row['cat_title'];
+                                echo "<tr>";
+                                echo "<td>{$cat_id}</td>";
+                                echo "<td>{$cat_title}</td>";
+                                echo "<tr>";
+                              } ?>
+                            </tbody>
+
+
+                          </table>
+                        </div>
                     </div>
                 </div>
                 <!-- /.row -->
